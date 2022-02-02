e0bafea959d39c29da01717ace419a680598a089
Ajagbe David <87986368+davidajagbe2004@users.noreply.github.com>
2022-02-02 11:37:18 +0100

    updated work

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-ReactDom.render(<h1 className="heading">Heloo world</h1>, ("root") )
+ReactDom.render(<h1> Good Morning </h1>, document.getElementById("root"));
